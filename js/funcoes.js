9df2406d4be2e455f79e7860ff9de54d64ecb945
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-09-17 07:59:55 -0300

    atualizando o codigo para atualidade

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,34 +1,52 @@
-function pegarValores() {
-            let n1 = parseFloat(document.getElementById("n1").value);
-            let n2 = parseFloat(document.getElementById("n2").value);
+function calcular(operacao){
+    const n1 = Number(document.getElementById('n1').value)
+    const n2 = Number(document.getElementById('n2').value)
 
-            // Se não for número, vira 0
-            if (isNaN(n1)) n1 = 0;
-            if (isNaN(n2)) n2 = 0;
 
-            return { n1, n2 };
-        }
+    let resultado
+    switch(operacao){
+        case '+':
+            resultado = somar(n1, n2);
+            break;
+        case '-':
+            resultado = subtrair(n1, n2);
+            break;
+        case '*':
+            resultado = multiplicar(n1, n2);
+            break;
+        case '/':
+            resultado = dividir(n1, n2);
+            break;
+    }
 
-        function somar() {
-            const { n1, n2 } = pegarValores();
-            document.getElementById("resultado").innerText = "A soma dos números é: " + (n1 + n2);
-        }
+    document.getElementById('resultado').innerHTML = "O resultado é " + resultado
+}
 
-        function Subtrair() {
-            const { n1, n2 } = pegarValores();
-            document.getElementById("resultado").innerText = "A subtração dos números é: " + (n1 - n2);
-        }
+function somar(n1, n2){
+    return Number(n1) + Number(n2)
+}
 
-        function Multiplicar() {
-            const { n1, n2 } = pegarValores();
-            document.getElementById("resultado").innerText = "A multiplicação dos números é: " + (n1 * n2);
-        }
+function subtrair(n1, n2){
+    return Number(n1) - Number(n2)
+}
 
-        function Dividir() {
-            const { n1, n2 } = pegarValores();
-            if (n2 === 0) {
-                document.getElementById("resultado").innerText = "Não é possível dividir por 0!";
-            } else {
-                document.getElementById("resultado").innerText = "A divisão dos números é: " + (n1 / n2);
-            }
-        }
+function multiplicar(n1, n2){
+    return Number(n1) * Number(n2)
+}
+
+function dividir(n1, n2){
+    if(n2 == 0){
+        console.log('Não é possível dividir número por zero')
+        return null
+    } else {
+        return Number(n1) / Number(n2)
+    }
+}
+
+const somaExpressao = function(n1, n2){
+    return n1 + n2
+}
+
+const somaArrow = (n1, n2) => n1 + n2
+
+console.log( somaArrow(10, 2))
